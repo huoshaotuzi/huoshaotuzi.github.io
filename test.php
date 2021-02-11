d861883074334747b9e30ca989487b789dabf120
火烧兔子 <874811226@qq.com>
2021-02-11 23:15:26 +0800

    更新

--- test.php
+++ test.php
@@ -1,14 +1,2 @@
 <?php
 
-class IndexController
-{
-    public function sayHello()
-    {
-        echo 'hello';
-    }
-}
-
-$controller = 'IndexController';
-$obj = new $controller;
-
-$obj->sayHello();
